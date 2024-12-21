6755fc7a03f9e12494247e886eaaa1a71bd1fdeb
DESKTOP-4PBISAO\Anudeep <anudeepramadugu@gmail.com>
2024-12-21 07:24:04 +0530

    Implementation of Tank Movement using MVC

--- Assets/Scripts/TankController.cs
+++ Assets/Scripts/TankController.cs
@@ -6,15 +6,36 @@ public class TankController
     private TankModel tankModel;
     private TankView tankView;
 
+    private Rigidbody rb;
+
 
     public TankController(TankModel model, TankView view)
     {
         tankModel = model;
-        tankView = view;
+        tankView = GameObject.Instantiate<TankView>(view);
+        rb = tankView.GetRigidbody();
 
         tankModel.SetTankController(this);
-        tankView.SetTankController(this);
+        tankView.SetTankController(this);        
+    }
 
-        GameObject.Instantiate(tankView.gameObject);        
+
+    public void Move(float movement, float movementSpeed)
+    {
+        rb.velocity = tankView.transform.forward * movement * movementSpeed;
+    }
+
+
+    public void Rotate(float rotation, float rotationSpeed)
+    {
+        Vector3 rotationVector = new Vector3(0, rotation * rotationSpeed, 0);
+        Quaternion deltaRotation = Quaternion.Euler(rotationVector * Time.deltaTime);
+        rb.MoveRotation(rb.rotation * deltaRotation);
+    }
+
+
+    public TankModel GetTankModel()
+    {
+        return tankModel;
     }
 }
--- Assets/Scripts/TankView.cs
+++ Assets/Scripts/TankView.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 
@@ -7,9 +8,49 @@ public class TankView : MonoBehaviour
 {
     private TankController tankController;
 
+    private float movementInput;
+    private float rotationInput;
+
+    [SerializeField] private Rigidbody rb;
+
+
+    private void Start()
+    {
+        SetCameraFollow();
+    }
+
+    private void SetCameraFollow()
+    {
+        Camera cam = Camera.main;
+        cam.gameObject.transform.SetParent(transform);
+        cam.transform.position = new Vector3(0, 3f, -5f);
+        cam.transform.rotation = Quaternion.Euler(15f, 0, 0);
+    }
+
+    private void Update()
+    {
+        Input();
+
+        if (movementInput != 0) tankController.Move(movementInput, tankController.GetTankModel().movementSpeed);
+        if (movementInput != 0 && rotationInput != 0) tankController.Rotate(rotationInput, tankController.GetTankModel().rotationSpeed);
+    }
+
+
+    private void Input()
+    {
+        movementInput = UnityEngine.Input.GetAxis("Vertical");
+        rotationInput = UnityEngine.Input.GetAxis("Horizontal");
+    }
+
 
     public void SetTankController(TankController controller)
     {
         tankController = controller;
+    }
+
+
+    public Rigidbody GetRigidbody()
+    {
+        return rb; 
     }
 }
